76dbd68307269b9ba1dfe3e37db852e24eed6494
michaeld55 <michaeld55@hotmail.co.uk>
2020-02-21 14:21:49 +0000

    added pets sold tests

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -8,8 +8,28 @@ def pet_shop_name( name )
 
 end
 
-def total_cash( money)
+def total_cash( money )
 
   return @pet_shop[:admin][:total_cash]
+
+end
+
+def add_or_remove_cash ( name, money )
+
+  # binding.pry
+  return @pet_shop[:admin][:total_cash] += money
+
+
+end
+
+def pets_sold( pets )
+
+  return @pet_shop[:admin][:pets_sold]
+
+end
+
+def increase_pets_sold( pets, amount )
+
+  return @pet_shop[:admin][:pets_sold] += amount
 
 end
